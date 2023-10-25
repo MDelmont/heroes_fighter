1a490b772f137dd6fd9cb640744eb1fac0516166
Zozo <benbar2412@gmail.com>
2023-10-25 17:07:59 +0200

    MAJ Datas pour fetch correctement les données (tous les persos possédant un A dans leur nom)

---
+++ src/assets/js/datas.js
@@ -0,0 +1,11 @@
+let env = require("../data/auth.json");
+
+export const data = async () => {
+  return fetch(`https://superheroapi.com/api.php/${env.token}/search/a`, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log("blabla", error);
+    });
+};
