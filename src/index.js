fb05d4be81ebc3051804ea6858e192ca2cba61b0
Zozo <benbar2412@gmail.com>
2023-10-26 12:21:07 +0200

    Mis en forme de l'index

--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@ import "./assets/scss/table.scss";
 import "./assets/scss/formTab.scss";
 import "./assets/scss/buttons.scss";
 
+// CSS
+import "./assets/scss/buttons.scss";
+import "./assets/scss/font.scss";
+import "./assets/scss/nav.scss";
+import "./assets/scss/select_heroes.scss";
+import "./assets/scss/table.scss";
+import "./assets/scss/modal.scss";
+
+// JS
 import "./assets/js/app";
 import "./assets/js/listener";
